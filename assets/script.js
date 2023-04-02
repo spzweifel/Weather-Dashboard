eef50d8ef1ca33da2eaa4e1f421705695cf32465
Sean Zweifel <spzweifel@gmail.com>
2023-04-02 19:47:27 -0400

    the API key says invalid even when it's new

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,38 @@
 // use fetch to get the data for the weather stuff
+
+//create input box to get city name. save that value as a variable. 
+
+//create query string. have to get lat and long to get the weather. do two api calls. first, takes the city name and gives lat and long in the textbox. take that return to do the weather api call. 
+
+//
+
+var userForm = document.querySelector('#user-form')
+var cityInput = document.querySelector('#cityname')
+
+
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={4a46a814423b539ffe06ab6c3ce3d0c7}"
+
+    // fetch(requestUrl)
+    //     .then(function (response) {
+    //         return response.json()
+    //     })
+    //     .then(function (data) {
+    //         console.log(data)
+    //     })
+
+
+
+
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault()
+
+    var cityname = cityInput.value.trim()
+
+    if (cityname) {
+        getForecastData(cityname)
+    }
+}
+
+
