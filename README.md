0e874dae700b8bbef846c9d1818a9f3abc178dae
Sean Zweifel <spzweifel@gmail.com>
2023-04-05 19:52:30 -0400

    worked on javascript

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Weather-Dashboard
+
+## Description
+
+In this assignment, I was tasked with creating a webpage from scratch to display a 5 day weather forecast. When the city name is typed in, the program should display the weather for the current day and the 5 following days and then save the city names.
+
+## License
+
+MIT License
+
+## Screenshots
+
+
+
+## Link to deployed webpage
--- assets/script.js
+++ assets/script.js
@@ -1,76 +1,81 @@
 // use fetch to get the data for the weather stuff
 
-//create input box to get city name. save that value as a variable. 
+//create input box to get city name. save that value as a variable.
 
-//create query string. have to get lat and long to get the weather. do two api calls. first, takes the city name and gives lat and long in the textbox. take that return to do the weather api call. 
+//create query string. have to get lat and long to get the weather. do two api calls. first, takes the city name and gives lat and long in the textbox. take that return to do the weather api call.
 
 //add an event listener on the search button
-
-//have to reference 
-var today = $('.box')
-var day1 = $('.day-box1')
-var day2 = $('.day-box2')
-var day3 = $('.day-box3')
-var day4 = $('.day-box4')
-var day5 = $('.day-box5')
-var userForm = document.querySelector('#user-form')
-var cityInput = document.querySelector('#cityname')
-var search = document.getElementById('btn')
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4a46a814423b539ffe06ab6c3ce3d0c7"
-//var lat = 
+$(function(){
 
 
+//have to reference
+var today = $(".box");
+var day1 = $(".day-box1");
+var day2 = $(".day-box2");
+var day3 = $(".day-box3");
+var day4 = $(".day-box4");
+var day5 = $(".day-box5");
+var userForm = $("#user-form");
+var cityInput = $("#cityname");
+var search = $(".btn");
 
-function getApi(event, city) {
-    event.preventDefault()
+var apiKey = "4a46a814423b539ffe06ab6c3ce3d0c7"
+//var lat =
 
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?" + city + "&appid=4a46a814423b539ffe06ab6c3ce3d0c7"
-    console.log(requestUrl)
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            var title = data.city.name + " " + dayjs().format("MM/DD/YYYY")
-            today.children().eq(0).text(title)
-            var cityList = data.list
+//addeventlistener for the city and console logs it
 
-            $("img").remove()
+function getApi(event) {
+  event.preventDefault();
 
-            for (var i = 0; i < cityList.length; i++)
-                var main = cityList[i].main
-                var temp = Math.ceil((9 / 5) * (main.temp - 273) + 32)
-                var humidity = main.humidity + "%"
-                var windSpeed = cityList[i].wind.speed + "mph"
-                var icon = cityList[i].weather[0].icon
+  var city = cityInput.value;
+  console.log(city)
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-                var img = $("<img>").attr(
-                    "src", "https://openweathermap.org/img/wn" + icon + "@2x.png")
-                
-                })
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
 
-            console.log(data.city.coord.lat)
-
-}
-
-
-//why is this not reading in the console?
-//search.addEventListener("click", getApi);
+        var currentCard = `
+        <h4 class="">temp: ${data.main.temp}</h4>
+                <p>hum: </p>`
 
 
+    //   var title = data.city.name + " " + dayjs().format("MM/DD/YYYY");
+    //   today.children().eq(0).text(title);
+    //   var cityList = data.list;
 
+    //   $("img").remove();
 
+    //   for (var i = 0; i < cityList.length; i++) {
+    //     var main = cityList[i].main;
+    //     var temp = Math.ceil((9 / 5) * (main.temp - 273) + 32);
+    //     var humidity = main.humidity + "%";
+    //     var windSpeed = cityList[i].wind.speed + "mph";
+    //     var icon = cityList[i].weather[0].icon;
 
-
-
-
-
+    //     var img = $("<img>").attr(
+    //       "src",
+    //       "https://openweathermap.org/img/wn" + icon + "@2x.png"
+    //     );
+    //   }
 
       
+    });
+}
+
+//why is this not reading in the console?
+search.addEventListener("click", getApi);
 
 
 
 
+function cityRender() {
+    
+}
+cityRender();
 
 // var formSubmitHandler = function (event) {
 //     event.preventDefault()
@@ -82,15 +87,13 @@ function getApi(event, city) {
 //     }
 // }
 
+today.eq(0).text(dayjs().format("MM/DD/YYYY"));
+day1.eq(0).text(dayjs().add(1, "day").format("MM/DD/YYYY"));
+day2.eq(0).text(dayjs().add(2, "day").format("MM/DD/YYYY"));
+day3.eq(0).text(dayjs().add(3, "day").format("MM/DD/YYYY"));
+day4.eq(0).text(dayjs().add(4, "day").format("MM/DD/YYYY"));
+day5.eq(0).text(dayjs().add(5, "day").format("MM/DD/YYYY"));
 
 
 
-
-
-
-today.eq(0).text(dayjs().format("MM/DD/YYYY"))
-day1.eq(0).text(dayjs().add(1, "day").format("MM/DD/YYYY"))
-day2.eq(0).text(dayjs().add(2, "day").format("MM/DD/YYYY"))
-day3.eq(0).text(dayjs().add(3, "day").format("MM/DD/YYYY"))
-day4.eq(0).text(dayjs().add(4, "day").format("MM/DD/YYYY"))
-day5.eq(0).text(dayjs().add(5, "day").format("MM/DD/YYYY"))
+})
